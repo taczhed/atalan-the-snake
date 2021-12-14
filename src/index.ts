0064c2a7b3f35a611096e290fd86587e7626496a
taczhed <patryklach99@gmail.com>
2021-12-14 20:29:55 +0100

    Points collecting mechanism

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-// 52x24 map size
+// 36x22 map size
 import Area from './modules/Area'
 import Snake from './modules/Snake'
 import defaultMap from "../assets/maps/default.json"
+import Point from "./modules/Point";
 
 //variables
 const snakeSize = 8
 const areaSize = 20
+const mapWidth = 36
+const mapHeight = 22
+let points = 0
 let snakeCords = [8, 10] //y, x
 let currentDestiantion = 'bottom'
 let gameMapScheme = defaultMap.gameMap
-let arrayOfMapAreas: Array<Array< Area | number >> = gameMapScheme
+let arrayOfMapAreas: Array<Array< Area | Point | number >> = gameMapScheme
 let snakeInterval: NodeJS.Timer
 let snakeTimeOut: NodeJS.Timeout
 let snakeBody: Array<Snake> = []
@@ -36,37 +40,41 @@ const app = {
     setSnakeInterval: (): void => {
         snakeTimeOut = setTimeout(() => (
             snakeInterval = setInterval(() => {
-                    if (currentDestiantion === 'top' && gameMapScheme[snakeCords[0] - 1][snakeCords[1]] === 0) {
+                    if (currentDestiantion === 'top' && typeof gameMapScheme[snakeCords[0] - 1][snakeCords[1]] === 'number') {
                         snakeBody[0].rotateHead('top')
                         if (app.isBodyHittedByHead('top')) app.endGame()
                         else {
+                            if (arrayOfMapAreas[snakeCords[0] - 1][snakeCords[1]] === 1) app.collectPoint(snakeCords[0] - 1, snakeCords[1], 'top')
                             app.moveAllSnakeBody()
                             snakeBody[0].snakeMove('y', -areaSize)
                             app.MatchTextures()
                             snakeCords[0] -= 1
                         }
-                    } else if (currentDestiantion === 'bottom' && gameMapScheme[snakeCords[0] + 1][snakeCords[1]] === 0) {
+                    } else if (currentDestiantion === 'bottom' && typeof gameMapScheme[snakeCords[0] + 1][snakeCords[1]] === 'number') {
                         snakeBody[0].rotateHead('bottom')
                         if (app.isBodyHittedByHead('bottom')) app.endGame()
                         else {
+                            if (arrayOfMapAreas[snakeCords[0] + 1][snakeCords[1]]) app.collectPoint(snakeCords[0] + 1, snakeCords[1], 'bottom')
                             app.moveAllSnakeBody()
                             snakeBody[0].snakeMove('y', areaSize)
                             app.MatchTextures()
                             snakeCords[0] += 1
                         }
-                    } else if (currentDestiantion === 'left' && gameMapScheme[snakeCords[0]][snakeCords[1] - 1] === 0) {
+                    } else if (currentDestiantion === 'left' && typeof gameMapScheme[snakeCords[0]][snakeCords[1] - 1] === 'number') {
                         snakeBody[0].rotateHead('left')
                         if (app.isBodyHittedByHead('left')) app.endGame()
                         else {
+                            if (arrayOfMapAreas[snakeCords[0]][snakeCords[1] - 1]) app.collectPoint(snakeCords[0], snakeCords[1] - 1, 'left')
                             app.moveAllSnakeBody()
                             snakeBody[0].snakeMove('x', -areaSize)
                             app.MatchTextures()
                             snakeCords[1] -= 1
                         }
-                    } else if (currentDestiantion === 'right' && gameMapScheme[snakeCords[0]][snakeCords[1] + 1] === 0) {
+                    } else if (currentDestiantion === 'right' && typeof gameMapScheme[snakeCords[0]][snakeCords[1] + 1] === 'number') {
                         snakeBody[0].rotateHead('right')
                         if (app.isBodyHittedByHead('right')) app.endGame()
                         else {
+                            if (arrayOfMapAreas[snakeCords[0]][snakeCords[1] + 1]) app.collectPoint(snakeCords[0], snakeCords[1] + 1, 'right')
                             app.moveAllSnakeBody()
                             snakeBody[0].snakeMove('x', areaSize)
                             app.MatchTextures()
@@ -76,8 +84,8 @@ const app = {
                         app.endGame()
                     }
                 pressAccess = true
-            }, 300)
-        ), 300)
+            }, 250)
+        ), 250)
     },
 
     renderSnake: () => {
@@ -131,11 +139,49 @@ const app = {
         })
     },
     endGame: () => {
-        console.log("END!")
         clearTimeout(snakeTimeOut)
         clearInterval(snakeInterval)
+        // alert('You lose!')
+    },
+    renderPoint: () => {
+        let loop = true
+        while (loop === true) {
+            const x = Math.floor(Math.random() * (mapWidth - 1))
+            const y = Math.floor(Math.random() * (mapHeight - 1))
+            let areCoordsFreeForPoint = true
+            snakeBody.forEach(bodyItem => {
+                if (bodyItem.x === x * areaSize || bodyItem.y === y * areaSize) areCoordsFreeForPoint = false
+            })
+            if (!arrayOfMapAreas[y][x] && areCoordsFreeForPoint) {
+                const point = new Point(x * areaSize, y * areaSize, 'point')
+                arrayOfMapAreas[y][x] = 1
+                gameMap.appendChild(point.body)
+                loop = false
+            }
+        }
+    },
+    collectPoint: (y: number, x: number, direction: string) => {
+        points += 1
+        console.log(points)
+        arrayOfMapAreas[y][x] = 0
+        const pointsDOM = document.querySelector('.point')
+        pointsDOM.remove()
+
+        let snakeBodyItem: Snake
+
+        if (direction === 'top') snakeBodyItem = new Snake(snakeBody[snakeBody.length - 1].x, snakeBody[snakeBody.length - 1].y + 20, 'ass', snakeBody[snakeBody.length - 1].direction)
+        else if (direction === 'bottom') snakeBodyItem = new Snake(snakeBody[snakeBody.length - 1].x, snakeBody[snakeBody.length - 1].y - 20, 'ass', snakeBody[snakeBody.length - 1].direction)
+        else if (direction === 'left') snakeBodyItem = new Snake(snakeBody[snakeBody.length - 1].x + 20, snakeBody[snakeBody.length - 1].y, 'ass', snakeBody[snakeBody.length - 1].direction)
+        else if (direction === 'right') snakeBodyItem = new Snake(snakeBody[snakeBody.length - 1].x - 20, snakeBody[snakeBody.length - 1].y, 'ass', snakeBody[snakeBody.length - 1].direction)
+
+        snakeBody[snakeBody.length - 1].changeBodyType('ass', 'body', snakeBody[snakeBody.length - 1].direction)
+        snakeBody.push(snakeBodyItem)
+        gameMap.appendChild(snakeBodyItem.body)
+
+        app.renderPoint()
     }
 }
 
 app.renderMapOfTheGame()
 app.renderSnake()
+app.renderPoint()
---
+++ src/modules/Point.ts
@@ -0,0 +1,13 @@
+import Area from "./Area";
+
+class Point extends Area {
+
+    constructor(x: number, y: number, type: string) {
+        super(x, y, type)
+        this.body.setAttribute('src' , '../../assets/textures/point.png')
+        this.body.classList.add('point')
+    }
+}
+
+
+export default Point
